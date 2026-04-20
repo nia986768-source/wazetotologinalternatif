2cb2e9220e28506866f3bf0309b9f620b215953a
nia986768-source <sutrisanti99@gmail.com>
2026-04-20 16:59:39 +0700

    fix: ganti logo & favicon ke link ImageKit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 const D = {
   brand: 'WAZETOTO',
   domain: 'wazetotologinalternatif.com',
-  logo: 'https://i.imgur.com/lubVHpM.png',
+  logo: 'https://ik.imagekit.io/md2fquuug/waze_pp.png',
   linkLogin: 'https://loginwap.gelapsekali.com/waze',
   linkDaftar: 'https://daftarwap.gelapsekali.com/waze',
   amphtml: 'wazetotologinalternatif.com',
